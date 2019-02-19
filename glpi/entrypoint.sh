3d3acf51e32a4b45ab79ff969d6b3fcc0b424210
in_dow <in_dow@hotmail.com>
2019-02-20 04:02:56 +0900

    GLPI: Fix subdirectory to glpi

--- glpi/entrypoint.sh
+++ glpi/entrypoint.sh
@@ -8,9 +8,22 @@
 MYSQLCMDBASE="mysql --host=${DB_HOST} --port=${DB_PORT:-3306} -ns --user=${DB_USERNAME} --password=${DB_PASSWORD} --database=${DB_DATABASE}"
 $MYSQLCMDBASE -w --connect-timeout=100 -e "SELECT 'OK';" ||  echo "Failed to access ${DB_HOST}:${DB_PORT:-3306}" || exit 1;
 #if [ `$MYSQLCMDBASE -w --connect-timeout=100 -e "show tables;" | wc -l` -eq "0" ] ; then
-if [ ! -f "config/config_db.php" ] ; then
+# setup for glpi
+if [ ! -f "/var/www/html/glpi/config/config_db.php" ] ; then
   echo Setup first data
-  sudo -u www-data -- php scripts/cliinstall.php --host=${DB_HOST}:${DB_PORT:-3306} --user=${DB_USERNAME} --pass=${DB_PASSWORD} --db=${DB_DATABASE} --lang=${INIT_LANG:-en_US}
+  sudo -u www-data -- php /var/www/html/glpi/scripts/cliinstall.php --host=${DB_HOST}:${DB_PORT:-3306} --user=${DB_USERNAME} --pass=${DB_PASSWORD} --db=${DB_DATABASE} --lang=${INIT_LANG:-en_US}
+  mkdir -p /var/lib/glpi/_cron
+  mkdir -p /var/lib/glpi/_dumps
+  mkdir -p /var/lib/glpi/_graphs
+  mkdir -p /var/lib/glpi/_lock
+  mkdir -p /var/lib/glpi/_pictures
+  mkdir -p /var/lib/glpi/_plugins
+  mkdir -p /var/lib/glpi/_rss
+  mkdir -p /var/lib/glpi/_sessions
+  mkdir -p /var/lib/glpi/_tmp
+  mkdir -p /var/lib/glpi/_uploads
+  mkdir -p /var/lib/glpi/_cache
+  chown -R www-data: /var/lib/glpi
 fi
 
 
